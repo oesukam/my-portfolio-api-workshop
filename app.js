6364a92ad2076cb12c0e184308919a4578530774
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-07-27 15:16:46 +0200

    setup express

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import { errors } from 'celebrate';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(errors());
+
+app.use((req, res, next) => {
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  const status = error.status || 500;
+  return res.status(status).json({
+    status,
+    message: error.message || 'Server error'
+  });
+});
+
+export default app;
---
+++ server.js
@@ -0,0 +1,5 @@
+import app from './app';
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}...`));
